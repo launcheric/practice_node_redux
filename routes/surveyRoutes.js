ce224fe2f588db078004f1e194ed957d058328ba
Launcheric <wangmacanghao@yahoo.com>
2018-06-27 15:32:23 -0400

    add mailer, and disable ad-block

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -13,7 +15,12 @@ module.exports = app => {
       subject,
       body,
       recipients: recipients.split(",").map(email => ({ email })),
-      _user: req.user.id
+      _user: req.user.id,
+      dateSent: Date.now()
     });
+
+    //Send email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
