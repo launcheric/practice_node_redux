d252175b70960ab630a8521c5f66353d9d1fb492
Launcheric <wangmacanghao@yahoo.com>
2018-07-02 23:56:29 -0400

    create survey field

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,22 +1,43 @@
 // SurveyForm shows a form for a user to add input
+import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient", name: "emails" }
+];
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field tyep="text" name="title" component={SurveyField} />
-      </div>
-    );
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
   }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          {this.renderFields}
-          <button type="submit">Submit</button>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
