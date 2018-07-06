008de8f3e376697b337659dd75b35b6a73f5d748
Launcheric <wangmacanghao@yahoo.com>
2018-07-06 10:37:15 -0400

    158 Advancing From SurveyForm

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,25 @@
 // SurveyNew show SurveyForm and SurveyFormReview
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
-  render() {
+  state = { showFormReview: false };
+
+  rederContent() {
+    if (this.state.showFormReview === true) {
+      return <SurveyFormReview />;
+    }
+
     return (
-      <div>
-        <SurveyForm />
-      </div>
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
     );
+  }
+
+  render() {
+    return <div>{this.rederContent()}</div>;
   }
 }
 
