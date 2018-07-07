c49dcd6d3bdc28e2782ed6bf1b96010d54a15983
LaunchEric <wangmacanghao@yahoo.com>
2018-07-07 00:08:36 -0400

    167 Redirect on Submit

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,32 +1,35 @@
 // SurveyFormReview shows users their from input for review
-import _ from 'lodash'
+import _ from "lodash";
 import React from "react";
-import { connect } from 'react-redux'
-import formFields from './formFields'
-import * as actions from '../../actions'
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
-  const reviewFields = _.map(formFields, ({ name, label}) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
         <label>{label}</label>
-        <div>
-          {formValues[name]}
-        </div>
+        <div>{formValues[name]}</div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
         Back
       </button>
       <button
-        onClick={() => submitSurvey(formValues)}
-        className='green btn-flat right white-text'>
+        onClick={() => submitSurvey(formValues, history)}
+        className="green btn-flat right white-text"
+      >
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -38,7 +41,9 @@ function mapStateToProps(state) {
   return {
     formValues: state.form.surveyForm.values
   };
-
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(SurveyFormReview));
