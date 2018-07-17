93e908ebafec9ca22f8c9cc96ce0636a60f46277
LaunchEric <wangmacanghao@yahoo.com>
2018-07-17 13:57:44 -0400

    190 191 Wiring Surveys Up to the Redux

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,13 +9,14 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
-module.exports = app =>
-  app.get('/api/surveys', requireLogin,  async (req, res) =>{
-     const surveys = await Survey.find({ _user: req.user.id })
-      .select({ recipients: false });
+module.exports = app => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
 
-     res.send(surveys);
-  })
+    res.send(surveys);
+  });
 
   app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
@@ -48,7 +49,8 @@ module.exports = app =>
             lastResponed: new Date()
           }
         );
-      }).exec()
+      })
+      .exec()
       .value();
 
     res.send({});
@@ -81,7 +83,4 @@ module.exports = app =>
       res.status(422).send(err);
     }
   });
-
-
-
 };
